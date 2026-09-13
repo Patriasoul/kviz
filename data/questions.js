0fae8aa06f5b3cf5d308a846311e5770d4afe0d5
Patriasoul <patriasoul@protonmail.com>
2026-09-13 21:10:07 +0200

    Add initial quiz question bank

---
+++ data/questions.js
@@ -0,0 +1,12 @@
+window.PATRIA_SOUL_QUESTIONS = [
+ {category:'opce',question:'Koji je glavni grad Republike Hrvatske?',answers:['Split','Zagreb','Rijeka','Osijek'],correctIndex:1},
+ {category:'opce',question:'Koji se datum obilježava kao Dan državnosti Republike Hrvatske?',answers:['30. svibnja','5. kolovoza','18. studenoga','8. listopada'],correctIndex:0},
+ {category:'povijest',question:'Koje je pismo posebno povezano s hrvatskom srednjovjekovnom kulturnom baštinom?',answers:['Glagoljica','Runica','Ćirilica','Latinica'],correctIndex:0},
+ {category:'povijest',question:'Koji je kralj okrunjen 925. godine prema tradicionalnom hrvatskom povijesnom računanju?',answers:['Tomislav','Zvonimir','Petar Krešimir IV.','Dmitar Zvonimir'],correctIndex:0},
+ {category:'domovinski-rat',question:'Koji se grad posebno obilježava 18. studenoga?',answers:['Vukovar','Pula','Karlovac','Dubrovnik'],correctIndex:0},
+ {category:'domovinski-rat',question:'Operacija Oluja započela je 1995. godine?',answers:['1. svibnja','5. kolovoza','4. kolovoza','15. siječnja'],correctIndex:2},
+ {category:'geografija',question:'Koji je najviši vrh Hrvatske?',answers:['Dinara (Sinjal)','Sveto brdo','Vaganski vrh','Risnjak'],correctIndex:0},
+ {category:'geografija',question:'Koja je najduža rijeka koja cijelim tokom protječe Hrvatskom?',answers:['Kupa','Drava','Sava','Cetina'],correctIndex:3},
+ {category:'kultura',question:'Koji je grad poznat po Dioklecijanovoj palači?',answers:['Zadar','Split','Trogir','Šibenik'],correctIndex:1},
+ {category:'glagoljica',question:'Kako se zove poznati hrvatski glagoljski spomenik iz 11. stoljeća?',answers:['Bašćanska ploča','Vinodolski zakonik','Istarski razvod','Šibenska molitva'],correctIndex:0}
+];
