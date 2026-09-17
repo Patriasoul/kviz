f79e848954766c4c2df91ea917a8dc0ae4029a80
Patriasoul <patriasoul@protonmail.com>
2026-09-17 05:53:18 +0200

    feat: load city online question batches 2-9 in quiz pipeline

---
+++ src/data/cityQuestions.online.all.js
@@ -0,0 +1,21 @@
+import { CITY_ONLINE_QUESTIONS } from "./cityQuestions.online";
+import { CITY_ONLINE_QUESTIONS_BATCH_2 } from "./cityQuestions.online.batch2";
+import { CITY_ONLINE_QUESTIONS_BATCH_3 } from "./cityQuestions.online.batch3";
+import { CITY_ONLINE_QUESTIONS_BATCH_4 } from "./cityQuestions.online.batch4";
+import { CITY_ONLINE_QUESTIONS_BATCH_5 } from "./cityQuestions.online.batch5";
+import { CITY_ONLINE_QUESTIONS_BATCH_6 } from "./cityQuestions.online.batch6";
+import { CITY_ONLINE_QUESTIONS_BATCH_7 } from "./cityQuestions.online.batch7";
+import { CITY_ONLINE_QUESTIONS_BATCH_8 } from "./cityQuestions.online.batch8";
+import { CITY_ONLINE_QUESTIONS_BATCH_9 } from "./cityQuestions.online.batch9";
+
+export const CITY_ONLINE_QUESTIONS_ALL = [
+  ...CITY_ONLINE_QUESTIONS,
+  ...CITY_ONLINE_QUESTIONS_BATCH_2,
+  ...CITY_ONLINE_QUESTIONS_BATCH_3,
+  ...CITY_ONLINE_QUESTIONS_BATCH_4,
+  ...CITY_ONLINE_QUESTIONS_BATCH_5,
+  ...CITY_ONLINE_QUESTIONS_BATCH_6,
+  ...CITY_ONLINE_QUESTIONS_BATCH_7,
+  ...CITY_ONLINE_QUESTIONS_BATCH_8,
+  ...CITY_ONLINE_QUESTIONS_BATCH_9,
+];
