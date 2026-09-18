d89f137e20503174b73c834a5bb9c633d389d995
Patriasoul <patriasoul@protonmail.com>
2026-09-18 22:39:31 +0200

    Invalidate stale quiz app cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "patriasoul-kviz-v2";
+const CACHE_NAME = "patriasoul-kviz-v4";
 const APP_SHELL = ["/kviz/", "/kviz/index.html", "/kviz/manifest.webmanifest", "/kviz/icons/icon.svg"];
 
 self.addEventListener("install", (event) => {
